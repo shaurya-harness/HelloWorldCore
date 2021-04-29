9d4b2f7a61012859510c83b98dfce9811b86afe2
Eoin McAfee <eoin.mcafee@harness.io>
2021-04-29 10:27:58 +0100

    adding project structure

---
+++ CoreLibraries/StringLibrary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CoreLibraries
+{
+    public static class StringLibrary
+    {
+        public static string GetHelloWorldString()
+        {
+            return "Hello World";
+        }
+        
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+
+            char ch = str[0];
+            return char.IsUpper(ch);
+        }
+    }
+}
---
+++ RunHelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CoreLibraries;
+
+namespace RunHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(StringLibrary.GetHelloWorldString());
+        }
+    }
+}
